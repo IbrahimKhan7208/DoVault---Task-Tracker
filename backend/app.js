9b614441b7fac925cc3cb86d970219995ceeab47
Ibrahim <khanibrahim7208@gmail.com>
2025-05-05 02:57:23 +0530

    Update CORS for Render frontend

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,10 @@ const cookieParser = require('cookie-parser')
 const cors = require("cors")
 
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://dovault-frontend.onrender.com'],
+    credentials: true
+  }));
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
