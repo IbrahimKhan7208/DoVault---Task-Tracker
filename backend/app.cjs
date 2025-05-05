b60650eed3c0395ed71ef849ea7810d115290913
Ibrahim <khanibrahim7208@gmail.com>
2025-05-06 04:14:38 +0530

    Fix: install backen

--- backend/app.cjs
+++ backend/app.cjs
@@ -27,12 +27,13 @@ app.use('/api/users', userRoutes)
 app.use('/api/projects', projectRoutes)
 app.use('/api/tasks', taskRoutes)
 
-app.use(express.static(path.join(__dirname, 'frontend/dist')))
+const staticPath = path.join(__dirname, 'frontend', 'dist');
+app.use(express.static(staticPath));
 
-const indexPath = path.resolve(__dirname, 'frontend', 'dist', 'index.html');
-app.get('*', (req, res) => {
-  res.sendFile(indexPath);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(staticPath, 'index.html'));
 });
+
 
 app.listen(port , (req, res)=>{
     connectDB()
